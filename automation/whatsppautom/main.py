7e252a5ae55d450a39db9a9d43a305fedb0e8b88
Gautham <appskans2017@gmail.com>
2022-01-06 13:37:04 +0530

    pywhatkit issue fixed

--- automation/whatsppautom/main.py
+++ automation/whatsppautom/main.py
@@ -1,16 +1,21 @@
 import pywhatkit
+import time
+import pyautogui
+import keyboard as k
+
+def sendmsg(n,m,hr,mi):
+    pywhatkit.sendwhatmsg(n,m,hr,mi,24)
+    pyautogui.click(1050, 950)
+    time.sleep(2)
+    k.press_and_release('enter')
+    print("Message has been forwarded to",n)
 
 print("Please open whatsapp web in your browser to automate your message")
 print("Enter the reciever mobile number")
-n = input()
+n1 = input()
 print("Enter the message to be forwarded")
-m = input()
+m1 = input()
 print("Enter time (hr-min), newline wise")
-hr = int(input())
-mi = int(input())
-
-
-
-pywhatkit.sendwhatmsg(n,m,hr,mi)
-print("Message has been forwarded to",n)
-
+hr1 = int(input())
+mi1 = int(input())
+sendmsg(n1,m1,hr1,mi1)
