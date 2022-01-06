c8f56ad313f0a76fcf6c0b0a7d74ea7b0020e740
Gautham <appskans2017@gmail.com>
2022-01-06 13:41:07 +0530

    another commit

--- automation/whatsppautom/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-print("Please open whatsapp web in your browser to automate your message")
-print("Enter the reciever mobile number")
-n = input()
-print("Enter the message to be forwarded")
-m = input()
-print("Enter time (hr-min), newline wise")
-hr = int(input())
-mi = int(input())
