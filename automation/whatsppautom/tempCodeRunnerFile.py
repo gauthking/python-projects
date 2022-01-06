906e8648a61522a3fdcc9ee59797802d341008c0
Gautham <appskans2017@gmail.com>
2022-01-06 13:37:55 +0530

    pywhatkit issue fixed

---
+++ automation/whatsppautom/tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+print("Please open whatsapp web in your browser to automate your message")
+print("Enter the reciever mobile number")
+n = input()
+print("Enter the message to be forwarded")
+m = input()
+print("Enter time (hr-min), newline wise")
+hr = int(input())
+mi = int(input())
