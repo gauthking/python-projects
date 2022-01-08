be8209a451a07907575c65caa310d445597f4a3c
Gautham <appskans2017@gmail.com>
2022-01-08 13:12:14 +0530

    new stuff added to pywhatkitproject

--- automation/whatsppautom/main.py
+++ automation/whatsppautom/main.py
@@ -2,20 +2,71 @@ import pywhatkit
 import time
 import pyautogui
 import keyboard as k
-
+# NOTEE:: In the sendmsg function, make sure you set the pyautogui position mouse keys in the whatsapp web text box... THE PREVIOUSLY SET VALUES WORKS ON MOST CASES.. but incase ..
+# called functions
+def ytplay(link):
+    try:
+        pywhatkit.playonyt(link)
+        print("Playing Now...")
+    except:
+        print("Network Error Occured... Please Make sure you are conncected to the internet..")
 def sendmsg(n,m,hr,mi):
-    pywhatkit.sendwhatmsg(n,m,hr,mi,24)
-    pyautogui.click(1050, 950)
-    time.sleep(2)
-    k.press_and_release('enter')
-    print("Message has been forwarded to",n)
+    try:
+        pywhatkit.sendwhatmsg(n,m,hr,mi,24)
+        pyautogui.click(1535, 985)
+        time.sleep(2)
+        k.press_and_release('enter')
+        print("Message has been forwarded to",n)
+    except:
+        print("Network Error Occured... Please Make sure you are conncected to the internet..")
 
-print("Please open whatsapp web in your browser to automate your message")
-print("Enter the reciever mobile number")
-n1 = input()
-print("Enter the message to be forwarded")
-m1 = input()
-print("Enter time (hr-min), newline wise")
-hr1 = int(input())
-mi1 = int(input())
-sendmsg(n1,m1,hr1,mi1)
+def googlesearch(search):
+    try:
+        pywhatkit.search(search)
+        print("Searching on google...")
+    except:
+        print("Network Error Occured... Please Make sure you are conncected to the internet..")
+
+def searchinfo(info1,lineno):
+    try:
+        pywhatkit.info(info1, lines = lineno)
+        print("Fetching info..")
+    except:
+        print("Network Error Occured... Please Make sure you are conncected to the internet..")
+
+
+def main():
+    print("Hello There... Welcome to PywhatKIT!")
+    print('')
+    print("Funcions Available \n 1. Send a WhatsApp Message \n 2. Perform a YouTube Serach \n 3. Perform a Google Search \n 4. Get info about a topic")
+    print('')
+    print('Please Enter which function you would like to perform(1/2/3/4):')
+    func = int(input())
+    if func == 1 :
+        print("Enter the reciever mobile number")
+        n1 = input()
+        print("Enter the message to be forwarded")
+        m1 = input()
+        print("Enter time (hr-min), newline wise")
+        hr1 = int(input())
+        mi1 = int(input())
+        sendmsg(n1,m1,hr1,mi1)
+
+    elif func == 2 :
+        print("Enter the URL/Title of the video to play on YouTube:")
+        play = input()
+        ytplay(play)
+
+    elif func == 3 :
+        print("Enter what to search on Google")
+        gsearch = input()
+        googlesearch(gsearch)
+
+    elif func == 4 :
+        print("Enter what info to fetch from the INTERNET:")
+        inf = input()
+        print("Length of the fetched info:")
+        line = int(input())
+        searchinfo(inf,line)
+
+main()
